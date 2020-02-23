40a6a2ae99a5c7456358e100ba4f61cd639ab9f9
Wolf1938 <1903439@uad.ac.uk>
2020-02-23 14:42:23 +0000

    Authentication updated

--- Kraken Bot/AuthenticateForm.Designer.cs
+++ Kraken Bot/AuthenticateForm.Designer.cs
@@ -33,6 +33,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Title = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LoginField = new System.Windows.Forms.Label();
+            this.PassWordField = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,9 +52,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(125, 242);
+            this.textBox1.Location = new System.Drawing.Point(125, 254);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(150, 20);
             this.textBox1.TabIndex = 2;
@@ -62,7 +64,8 @@
             this.Title.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Title.Font = new System.Drawing.Font("Wide Latin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(125, 165);
+            this.Title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
+            this.Title.Location = new System.Drawing.Point(125, 168);
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(150, 25);
             this.Title.TabIndex = 3;
@@ -71,19 +74,46 @@
             // 
             // textBox2
             // 
-            this.textBox2.BackColor = System.Drawing.Color.Gray;
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Location = new System.Drawing.Point(125, 268);
+            this.textBox2.Location = new System.Drawing.Point(125, 315);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(150, 20);
             this.textBox2.TabIndex = 4;
+            // 
+            // LoginField
+            // 
+            this.LoginField.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginField.Font = new System.Drawing.Font("Wide Latin", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
+            this.LoginField.Location = new System.Drawing.Point(125, 226);
+            this.LoginField.Name = "LoginField";
+            this.LoginField.Size = new System.Drawing.Size(150, 25);
+            this.LoginField.TabIndex = 5;
+            this.LoginField.Text = "Login";
+            this.LoginField.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LoginField.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PassWordField
+            // 
+            this.PassWordField.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PassWordField.Font = new System.Drawing.Font("Wide Latin", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassWordField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
+            this.PassWordField.Location = new System.Drawing.Point(122, 287);
+            this.PassWordField.Name = "PassWordField";
+            this.PassWordField.Size = new System.Drawing.Size(150, 25);
+            this.PassWordField.TabIndex = 6;
+            this.PassWordField.Text = "PassWord";
+            this.PassWordField.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // AuthenticateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.PassWordField);
+            this.Controls.Add(this.LoginField);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.Title);
             this.Controls.Add(this.pictureBox1);
@@ -102,5 +132,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label Title;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label LoginField;
+        private System.Windows.Forms.Label PassWordField;
     }
 }
--- Kraken Bot/AuthenticateForm.cs
+++ Kraken Bot/AuthenticateForm.cs
@@ -16,5 +16,10 @@ namespace Kraken_Bot
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
